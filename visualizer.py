a90f1dc47ad1f90bc44c4695c789616e487e4059
addisonwurtz <awurtz@pdx.edu>
2023-05-18 11:47:54 -0700

    Moved audiobar class to audiobar.py

--- visualizer.py
+++ visualizer.py
@@ -1,48 +1,9 @@
 import librosa
 import numpy as np
 import pygame
+from audiobar import AudioBar
 
 
-def clamp(min_value, max_value, value):
-
-    if value < min_value:
-        return min_value
-
-    if value > max_value:
-        return max_value
-
-    return value
-
-
-class AudioBar:
-
-    def __init__(self, x, y, freq, color, width=50, min_height=10, max_height=100, min_decibel=-80, max_decibel=0):
-
-        self.x, self.y, self.freq = x, y, freq
-
-        self.color = color
-
-        self.width, self.min_height, self.max_height = width, min_height, max_height
-
-        self.height = min_height
-
-        self.min_decibel, self.max_decibel = min_decibel, max_decibel
-
-        self.__decibel_height_ratio = (self.max_height - self.min_height)/(self.max_decibel - self.min_decibel)
-
-    def update(self, dt, decibel):
-
-        desired_height = decibel * self.__decibel_height_ratio + self.max_height
-
-        speed = (desired_height - self.height)/0.1
-
-        self.height += speed * dt
-
-        self.height = clamp(self.min_height, self.max_height, self.height)
-
-    def render(self, screen):
-
-        pygame.draw.rect(screen, self.color, (self.x, self.y + self.max_height - self.height, self.width, self.height))
 
 
 filename = "LanaDelRey-DietMountainDew(OfficialInstrumental).mp3"
@@ -72,7 +33,7 @@ pygame.init()
 
 infoObject = pygame.display.Info()
 
-screen_w = int(infoObject.current_w/2.5)
+screen_w = int(infoObject.current_w/1.5)
 screen_h = int(infoObject.current_w/2.5)
 
 # Set up the drawing window
